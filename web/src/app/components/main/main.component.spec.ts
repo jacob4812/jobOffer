1947ab0eeddb4cfbff9f8963020ccfa20b282647
uncommonnick <uncommonnick@gmail.com>
2024-07-31 21:24:11 +0200

    JOB-5

--- web/src/app/components/main/main.component.spec.ts
+++ web/src/app/components/main/main.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MainComponent } from './main.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 describe('MainComponent', () => {
@@ -27,24 +26,4 @@ describe('MainComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should toggle nav menu on button click', () => {
-    const button = debugElement.query(By.css('.navbar-toggler')).nativeElement;
-    button.click();
-    fixture.detectChanges();
-    expect(component.isNavOpen).toBeTrue();
-
-    button.click();
-    fixture.detectChanges();
-    expect(component.isNavOpen).toBeFalse();
-  });
-
-  it('should close nav menu on outside click', () => {
-    component.isNavOpen = true;
-    fixture.detectChanges();
-
-    document.dispatchEvent(new MouseEvent('mousedown', { bubbles: true }));
-    fixture.detectChanges();
-
-    expect(component.isNavOpen).toBeFalse();
-  });
 });
