78270cd2d0f2d2d3bfc37b4a77f02a37aa31df07
uncommonnick <uncommonnick@gmail.com>
2024-07-31 20:38:56 +0200

    JOB-5

--- web/src/app/components/main/main.component.ts
+++ web/src/app/components/main/main.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
-  standalone: true,
-  imports: [],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit, OnDestroy {
+  isNavOpen = false;
 
+  @ViewChild('navRef', { static: false }) navRef!: ElementRef;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
+
+  ngOnDestroy(): void {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
+  toggleNav(): void {
+    this.isNavOpen = !this.isNavOpen;
+    console.log('Toggle nav:', this.isNavOpen);
+  }
+
+  handleClickOutside = (event: MouseEvent): void => {
+    console.log('Click outside:', event.target);
+    if (this.navRef && !this.navRef.nativeElement.contains(event.target)) {
+      this.isNavOpen = false;
+    }
+  }
 }
