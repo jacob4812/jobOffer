bade3cb226883ded5b0b6e7e66bb65dd4dd70703
Jakub Kleszczynski <kleszczynski.jakub@gmail.com>
2024-07-31 22:43:30 +0200

    JOB-19 - offerservice

--- api/src/main/java/com/example/api/dto/UserDto.java
+++
@@ -1,3 +0,0 @@
-package com.example.api.dto;
-
-public record UserDto(Long id, String Login, String Password) {}
---
+++ api/src/main/java/pl/joboffer/job/dto/Offer.java
@@ -0,0 +1,3 @@
+package pl.joboffer.job.dto;
+
+public record Offer(Long id, String name, double salary, String description, String location) {}
