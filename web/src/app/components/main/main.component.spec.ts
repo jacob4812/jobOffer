888180ed9af39f97bce3a226bc7b47dddd9b000f
uncommonnick <uncommonnick@gmail.com>
2024-07-31 21:30:40 +0200

    JOB-5 (#11)
    * JOB-5 - strona startowa
    ---------
    
    Co-authored-by: jacob4812 <166031282+jacob4812@users.noreply.github.com>

--- web/src/app/components/main/main.component.spec.ts
+++ web/src/app/components/main/main.component.spec.ts
@@ -1,23 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MainComponent } from './main.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DebugElement } from '@angular/core';
 
 describe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MainComponent]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [MainComponent]
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(MainComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
