c76ec06a0e5bedf35955a38f348ed6b28204a6c4
uncommonnick <uncommonnick@gmail.com>
2024-08-01 14:54:30 +0200

    JOB-10

--- web/src/app/components/register/register.component.ts
+++ web/src/app/components/register/register.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, importProvidersFrom } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss'],
+  imports: [ReactiveFormsModule] 
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      repeatPassword: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Registration:', this.registerForm.value);
+      // Handle registration logic here
+    }
+  }
 }
