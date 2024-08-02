03b5ad7436d969a8cf172b2a36c21b6042a77699
Jakub Kleszczynski <kleszczynski.jakub@gmail.com>
2024-08-02 23:45:35 +0200

    JOB-22 - dodanie offer service i controller. na razie bez statusu

--- core/src/main/java/pl/joboffer/job/features/offer/OfferServiceImpl.java
+++ core/src/main/java/pl/joboffer/job/features/offer/OfferServiceImpl.java
@@ -1,20 +1,28 @@
 package pl.joboffer.job.features.offer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
 import pl.joboffer.job.dto.Offer;
 
+@Service
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class OfferServiceImpl implements OfferService {
   @NotNull OfferMapper offerMapper;
   @NotNull OfferRepository offerRepository;
 
-  @Override
-  public Offer findAllJobOffers() {
-    try {
-      var offerEntity = offerRepository.readAllBy();
+  public OfferServiceImpl(
+      OfferMapper offerMapper, OfferRepository offerRepository, ObjectMapper objectMapper) {
+    this.offerMapper = offerMapper;
+    this.offerRepository = offerRepository;
+  }
 
-      return offerMapper.mapEntityToDto(offerEntity);
-    } catch (Exception e) {
-      throw new RuntimeException();
-    }
+  @Override
+  public List<Offer> findAllJobOffers() {
+    var offerEntity = offerRepository.findAll();
+    return offerMapper.mapListEntityToDto(offerEntity);
   }
 }
