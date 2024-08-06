587039e5209c815599619ee559708b1d4aac2030
uncommonnick <uncommonnick@gmail.com>
2024-08-06 14:06:19 +0200

    Job 10 (#18)
    * JOB-10 - dodanie register service
    
    ---------
    
    Co-authored-by: Jakub Kleszczynski <kleszczynski.jakub@gmail.com>
    Co-authored-by: jacob4812 <166031282+jacob4812@users.noreply.github.com>

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -2,10 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration()
+    provideClientHydration(),
+    ReactiveFormsModule
   ]
 };
