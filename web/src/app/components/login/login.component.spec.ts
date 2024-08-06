4c69d3393983a33ae3c112109518a3cc722face3
uncommonnick <uncommonnick@gmail.com>
2024-08-06 14:00:17 +0200

    Job 9 (#17)
    * JOB-9
    
    * JOB-9 Skleroza/skolioza, spotless apply xD
    
    ---------
    
    Co-authored-by: jacob4812 <166031282+jacob4812@users.noreply.github.com>

--- web/src/app/components/login/login.component.spec.ts
+++ web/src/app/components/login/login.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,10 +9,16 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      declarations: [LoginComponent],
+      imports: [
+        FormsModule,
+        RouterModule.forRoot([])
+      ]
     })
-    .compileComponents();
+      .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- web/src/app/components/login/login.component.ts
+++ web/src/app/components/login/login.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
+  standalone: true,
+  imports: [ReactiveFormsModule]
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: [''],
+      password: ['']
+    });
+  }
+
+  onSubmit() {
+    console.log('Login:', this.loginForm.value);
+    // Handle login logic
+  }
 }
