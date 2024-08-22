a1a605c1351a4102dc4af1fb22ef236580a14c0d
jacob4812 <166031282+jacob4812@users.noreply.github.com>
2024-08-22 17:13:35 +0200

    JOB-35 - crud dla offer (#33)
    * JOB-35 - crud dla offer
    
    * JOB-35 - crud dla offer
    
    ---------
    
    Co-authored-by: Jakub Kleszczynski <kleszczynski.jakub@gmail.com>

--- core/src/main/java/pl/joboffer/job/features/offer/OfferController.java
+++ core/src/main/java/pl/joboffer/job/features/offer/OfferController.java
@@ -25,4 +25,19 @@ public class OfferController {
     Page<Offer> offerPage = offerService.findAllJobOffers(pageRequest);
     return ResponseEntity.ok(offerPage);
   }
+
+  @PostMapping("/addJobOffer")
+  public ResponseEntity<OfferEntity> addJobOffer(@RequestBody Offer offer) {
+    return ResponseEntity.ok(offerService.addJobOffer(offer));
+  }
+
+  @PutMapping("/editJobOffer")
+  public ResponseEntity<OfferEntity> editJobOffer(@RequestBody Offer offer) {
+    return ResponseEntity.ok(offerService.editJobOffer(offer));
+  }
+
+  @DeleteMapping("/deleteJobOffer/{id}")
+  public ResponseEntity<OfferEntity> deleteJobOffer(@PathVariable("id") Long id) {
+    return ResponseEntity.ok(offerService.deleteJobOffer(id));
+  }
 }
--- core/src/main/java/pl/joboffer/job/features/offer/OfferServiceImpl.java
+++ core/src/main/java/pl/joboffer/job/features/offer/OfferServiceImpl.java
@@ -1,6 +1,7 @@
 package pl.joboffer.job.features.offer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.AccessLevel;
@@ -28,5 +29,51 @@ public class OfferServiceImpl implements OfferService {
     Page<OfferEntity> offerPage = offerRepository.findAll(pageRequest);
     List<Offer> offers = offerMapper.mapListEntityToDto(offerPage.getContent());
     return new PageImpl<>(offers, pageRequest, offerPage.getTotalElements());
+  }
+
+  @Override
+  @Transactional
+  public OfferEntity addJobOffer(Offer offer) {
+    var savedOfferEntity = offerMapper.mapDtoToEntity(offer);
+    System.out.println(offer);
+    System.out.println(savedOfferEntity);
+    try {
+      var savedEntity = offerRepository.save(savedOfferEntity);
+    } catch (Exception e) {
+      throw new RuntimeException("nie da rady zapisac");
+    }
+    return savedOfferEntity;
+  }
+
+  @Override
+  @Transactional
+  public OfferEntity editJobOffer(Offer offer) {
+    if (offer.id() == null) {
+      throw new RuntimeException("Brak id dla wskazanej oferty");
+    }
+    var offerEntity = offerMapper.mapDtoToEntity(offer);
+    try {
+      var savedEntity = offerRepository.save(offerEntity);
+
+    } catch (RuntimeException e) {
+      throw e;
+    }
+    return offerEntity;
+  }
+
+  @Override
+  @Transactional
+  public OfferEntity deleteJobOffer(Long idOffer) {
+    var offerEntity =
+        offerRepository
+            .findById(idOffer)
+            .orElseThrow(() -> new RuntimeException("Brak ofert dla wskazanego id"));
+    try {
+      offerRepository.delete(offerEntity);
+    } catch (RuntimeException e) {
+      throw new RuntimeException("Usuwanie nie powiodlo sie");
+    }
+
+    return offerEntity;
   }
 }
