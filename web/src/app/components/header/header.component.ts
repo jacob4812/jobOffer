28d0768e7d5eafb06d00b88db457a014f79d341c
jacob4812 <166031282+jacob4812@users.noreply.github.com>
2024-09-10 17:38:32 +0200

    poprawki header i footer (#38)
    Co-authored-by: Jakub Kleszczynski <kleszczynski.jakub@gmail.com>

--- web/src/app/components/header/header.component.ts
+++ web/src/app/components/header/header.component.ts
@@ -20,7 +20,7 @@ export class HeaderComponent implements OnInit {
       { separator: true },
       { label: 'Dodaj CV' },
       { separator: true },
-      { label: 'Setup', icon: 'pi pi-cog', routerLink: ['/setup'] }
+      { label: 'Mój profil', icon: 'pi pi-cog', routerLink: ['/myprofile'] }
     ];
 
   }
