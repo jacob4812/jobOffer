aec0a1e55410ff129c9a1232b628edddb7ebdce8
uncommonnick <uncommonnick@gmail.com>
2024-09-10 10:52:05 +0200

    JOB-32

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {RouterModule, Routes} from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { RegisterCompanyComponent } from './components/registercompany/registercompany.component';
 import {NgModule} from "@angular/core";
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' },
   { path: 'login', component: LoginComponent  },
   { path: 'register', component: RegisterComponent },
+  { path: 'registercompany', component: RegisterCompanyComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
