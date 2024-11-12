9b28fd25edbcb3a9101546ff2ea11df0b6257bf1
Jakub Kleszczynski <kleszczynski.jakub@gmail.com>
2024-11-12 15:49:35 +0100

    JOB-36-poprawki

--- core/src/main/java/pl/joboffer/job/security/swagger/SpringDocConfig.java
+++ core/src/main/java/pl/joboffer/job/security/swagger/SpringDocConfig.java
@@ -3,19 +3,11 @@ package pl.joboffer.job.security.swagger;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.StringUtils;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SpringDocConfig {
   @Bean
   public GroupedOpenApi api() {
-    return GroupedOpenApi.builder()
-            .group("public-api")
-            .pathsToMatch("/**")
-            .build();
+    return GroupedOpenApi.builder().group("public-api").pathsToMatch("/**").build();
   }
-
 }
