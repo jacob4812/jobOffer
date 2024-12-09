eed76bde34cbd3a9bfed26af3d5bd540c609271f
Jakub Kleszczynski <kleszczynski.jakub@gmail.com>
2024-12-10 00:37:29 +0100

    JOB-46 - aplikowanie na oferte pracy, przypisuje do firmy po id i po id oferty, do zrobienia rozszerzenie api application , przeslanie danych aplikujacego do bazy i do aplikacji u firmy

---
+++ web/src/app/models/applicationRequest.model.ts
@@ -0,0 +1,4 @@
+export interface ApplicationRequest {
+    userId: number;
+    offerId: number;
+  }
--- web/src/services/application/application.service.ts
+++ web/src/services/application/application.service.ts
@@ -3,6 +3,7 @@ import { RestService } from '../rest/rest.service';
 import {Observable} from "rxjs";
 import {Page} from "../../app/models/page.model";
 import { Application } from 'src/app/models/application.model';
+import { ApplicationRequest } from 'src/app/models/applicationRequest.model';
 @Injectable({
   providedIn: 'root'
 })
@@ -14,5 +15,16 @@ private readonly applicationUrl = 'applications';
     const url = `${this.applicationUrl}/user/${userId}?page=${page}&size=${size}`;
     return this.restService.getPageable<Page<Application>>(url);
   }
-
+  applyForJob(applicationRequest:ApplicationRequest):Observable<Application>{
+    return this.restService.post(this.applicationUrl,applicationRequest);
+  }
 }
+// this.http.post('/api/applications', applicationRequest).subscribe({
+//   next: (response: any) => {
+//     alert("Application submitted successfully.");
+//     this.dialogRef.close(); // Close the dialog on success
+//   },
+//   error: (err) => {
+//     alert("Failed to submit application: " + err.message);
+//   }
+// });
