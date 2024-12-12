e6a46711267c0f59f004c1ad027fc92dacb6b2dc
uncommonnick <uncommonnick@gmail.com>
2024-12-12 20:10:42 +0100

    Walidacja formularza logowania (JOB-28), jak i rejestracji (JOB-27)

--- web/src/app/components/registercompany/registercompany.component.ts
+++ web/src/app/components/registercompany/registercompany.component.ts
@@ -1,36 +1,53 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-import {Router, RouterLink} from "@angular/router";
-import {RegisterCompanyService} from "../../../services/register/register-company.service";
+import { Router, RouterModule } from '@angular/router';
+import { RegisterCompanyService } from '../../../services/register/register-company.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   templateUrl: './registercompany.component.html',
   styleUrls: ['./registercompany.component.scss'],
-  imports: [ReactiveFormsModule, RouterLink]
+  imports: [ReactiveFormsModule, RouterModule, CommonModule]
 })
 export class RegisterCompanyComponent {
   registerForm: FormGroup;
 
-  constructor(private fb: FormBuilder,
-              private registerCompanyService: RegisterCompanyService,
-              private router: Router) {
-    this.registerForm = this.fb.group({
-      companyName: ['', Validators.required],
-      nip: ['', Validators.required],
-      phoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
-      username: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      repeatPassword: ['', Validators.required]
-    }, { validators: this.passwordMatchValidator });
+  constructor(
+    private fb: FormBuilder,
+    private registerCompanyService: RegisterCompanyService,
+    private router: Router
+  ) {
+    this.registerForm = this.fb.group(
+      {
+        companyName: ['', Validators.required],
+        nip: ['', Validators.required],
+        phoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+        username: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email, Validators.maxLength(250)]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(25),
+            Validators.pattern(
+              // Hasło musi zawierać co najmniej 1 dużą literę, 1 małą literę, 1 cyfrę i 1 znak specjalny
+              /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,25}$/
+            ),
+          ],
+        ],
+        repeatPassword: ['', Validators.required]
+      },
+      { validators: this.passwordMatchValidator }
+    );
   }
 
   private passwordMatchValidator(g: FormGroup) {
-    return g.get('password')!.value === g.get('repeatPassword')!.value
-      ? null : { 'mismatch': true };
+    return g.get('password')?.value === g.get('repeatPassword')?.value
+      ? null
+      : { mismatch: true };
   }
 
   onSubmit() {
--- web/src/services/auth/auth.service.ts
+++ web/src/services/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import {RestService} from "../rest/rest.service";
-import {catchError,Observable} from "rxjs";
+import { RestService } from "../rest/rest.service";
+import { Observable, throwError } from "rxjs";
+import { catchError, map } from "rxjs/operators";
 import { JwtHelperService } from "@auth0/angular-jwt";
 import { Router } from "@angular/router";
 
@@ -9,33 +10,42 @@ import { Router } from "@angular/router";
 })
 export class AuthService {
   invalidLogin = false;
-    helper = new JwtHelperService();
-  constructor(private router: Router,private restService:RestService) { }
+  helper = new JwtHelperService();
 
-  onLogin(form:any){
-    this.restService.post("login",form)
-      .pipe(
-        catchError(async (error) =>{
-        this.invalidLogin = true;
-        throw error;
-  }),
-      )
-      .subscribe((response)=>{
-        if(response){
-          this.invalidLogin = false
-            const decodedToken = this.helper.decodeToken(response.tokenContent);
-          console.log(response.tokenContent);
-          console.log(decodedToken);
-          localStorage.setItem("token",response.tokenContent);
-          localStorage.setItem("email",decodedToken.sub);
-          localStorage.setItem("idUser",decodedToken.idUser);
+  constructor(private router: Router, private restService: RestService) {}
+
+  onLogin(form: any): Observable<any> {
+    return this.restService.post("login", form).pipe(
+      map((response: any) => {
+        if (response) {
+          this.invalidLogin = false;
+          const decodedToken = this.helper.decodeToken(response.tokenContent);
+
+          localStorage.setItem("token", response.tokenContent);
+          localStorage.setItem("email", decodedToken.sub);
+          localStorage.setItem("idUser", decodedToken.idUser);
           localStorage.setItem("role", decodedToken.role[0].authority);
 
-
-        }
           this.router.navigate([""]);
+        }
+        return response;
+      }),
+      catchError((error) => {
+        this.invalidLogin = true;
+        console.error('Login error:', error);
+        return throwError(() => new Error('Nieprawidłowe dane logowania'));
+      })
+    );
+  }
 
-      });
+  isAuthenticated(): boolean {
+    const token = localStorage.getItem("token");
+    return token ? !this.helper.isTokenExpired(token) : false;
+  }
 
+
+  logout(): void {
+    localStorage.clear();
+    this.router.navigate(["/login"]);
   }
 }
