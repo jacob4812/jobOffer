8145aeb23f7ce7c0e186f3cbbd9c0a5577103fb6
uncommonnick <uncommonnick@gmail.com>
2024-12-28 19:53:57 +0100

    Spotless

--- api/src/main/java/pl/joboffer/job/dto/user/UserLoginDetails.java
+++ api/src/main/java/pl/joboffer/job/dto/user/UserLoginDetails.java
@@ -3,9 +3,4 @@ package pl.joboffer.job.dto.user;
 import javax.validation.constraints.NotNull;
 
 public record UserLoginDetails(
-        @NotNull String email,
-        String password,
-        String username,
-        Long phoneNumber) {}
-
-
+    @NotNull String email, String password, String username, Long phoneNumber) {}
