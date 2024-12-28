2fd3857e7ec452e3e9aa84d7acd5d2f5d9029f0f
uncommonnick <uncommonnick@gmail.com>
2024-12-28 20:01:11 +0100

    Spotless plugin nie chce mi sprawdzać w core xd

--- core/src/main/java/pl/joboffer/job/features/user/UserController.java
+++ core/src/main/java/pl/joboffer/job/features/user/UserController.java
@@ -11,34 +11,34 @@ import org.springframework.http.HttpStatus;
 @RequestMapping("/api/users")
 public class UserController {
 
-  private final UserEntityService userEntityService;
+    private final UserEntityService userEntityService;
 
-  public UserController(UserEntityService userEntityService) {
-    this.userEntityService = userEntityService;
-  }
-
-  @GetMapping("/profile")
-  public ResponseEntity<UserEntity> findUserByEmail(@RequestParam String email) {
-    UserEntity user = userEntityService.findUserByEmail(email);
-    return ResponseEntity.ok(user);
-  }
-
-  @PutMapping("/update")
-  public ResponseEntity<Void> editUser(@RequestBody @Valid UserLoginDetails userLoginDetails) {
-    UserEntity existingUser = userEntityService.findUserByEmail(userLoginDetails.email());
-
-    if (existingUser == null) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    public UserController(UserEntityService userEntityService) {
+        this.userEntityService = userEntityService;
     }
 
-    UserDetails userDetails = new UserDetails(
-            existingUser.getId(),
-            userLoginDetails.username() != null ? userLoginDetails.username() : existingUser.getLogin(),
-            userLoginDetails.phoneNumber() != null ? userLoginDetails.phoneNumber() : existingUser.getPhoneNumber(),
-            existingUser.getUserRole()
-    );
+    @GetMapping("/profile")
+    public ResponseEntity<UserEntity> findUserByEmail(@RequestParam String email) {
+        UserEntity user = userEntityService.findUserByEmail(email);
+        return ResponseEntity.ok(user);
+    }
 
-    userEntityService.editUser(userLoginDetails, userDetails);
-    return ResponseEntity.ok().build();
-  }
+    @PutMapping("/update")
+    public ResponseEntity<Void> editUser(@RequestBody @Valid UserLoginDetails userLoginDetails) {
+        UserEntity existingUser = userEntityService.findUserByEmail(userLoginDetails.email());
+
+        if (existingUser == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        UserDetails userDetails = new UserDetails(
+                existingUser.getId(),
+                userLoginDetails.username() != null ? userLoginDetails.username() : existingUser.getLogin(),
+                userLoginDetails.phoneNumber() != null ? userLoginDetails.phoneNumber() : existingUser.getPhoneNumber(),
+                existingUser.getUserRole()
+        );
+
+        userEntityService.editUser(userLoginDetails, userDetails);
+        return ResponseEntity.ok().build();
+    }
 }
